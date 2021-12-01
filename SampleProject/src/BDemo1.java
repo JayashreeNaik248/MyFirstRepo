64c8dc4f422c2cb31f7c4f5c63d6e5bb1506fb3d
JayashreeNaik248 <jayashreenaik146@gmail.com>
2021-12-01 21:16:58 +0530

    Still Learning

--- SampleProject/src/BDemo1.java
+++ SampleProject/src/BDemo1.java
@@ -4,6 +4,7 @@ public class BDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Iam Learning");
+		System.out.println("on the way");
 	}
 
 }
