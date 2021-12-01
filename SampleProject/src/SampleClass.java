2ccca1be301edbd175876085c757c598360b2da9
JayashreeNaik248 <jayashreenaik146@gmail.com>
2021-12-01 22:23:25 +0530

    commit

--- SampleProject/src/SampleClass.java
+++ SampleProject/src/SampleClass.java
@@ -3,5 +3,6 @@ public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hii All" );
 		System.out.println("let me add few codes");
+		System.out.println("Clearing Conflicts");
 	}
 }
